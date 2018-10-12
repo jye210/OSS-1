14807e2ae03ec0464be33ce33cdbbb9f7cdc86bd
JIYE <jye210@gmail.com>
2018-10-12 11:41:33 +0900

    9dan add

--- 99dan.cpp
+++ 99dan.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main()
 {
     int dan;
-    for(int dan = 2; dan < 9; dan++)
+    for(int dan = 2; dan < 10; dan++)
         for (int i = 1; i < 10; i++)
         {
             cout << dan << " * " << i << " = " << dan * i << endl;
